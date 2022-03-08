3ab8b58af5d5cfc7d763227d2a1a13e2c81ea0ec
MustafacanALTUNCU <mustafacanaltuncu0@gmail.com>
2022-03-08 19:25:07 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<T,U>
+    {
+        T[] _array;
+        T[] tempArray;
+        U[] _array1;
+        U[] tempArray1;
+       
+
+        public MyDictionary()
+        {
+            
+            _array = new T[0];
+            _array1 = new U[0];
+        }
+
+        public void Add(T key, U value) { 
+            bool varmi = _array.Contains(key);
+            if (varmi)
+            {
+                Console.WriteLine("Zaten eklendi");
+            }
+            else
+            {
+
+                tempArray = _array;
+                _array = new T[_array.Length + 1];
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    _array[i] = tempArray[i];
+
+                }
+                _array[_array.Length - 1] = key;
+                tempArray1 = _array1;
+                _array1 = new U[_array1.Length + 1];
+                for (int i = 0; i < tempArray1.Length; i++)
+                {
+                    _array1[i] = tempArray1[i];
+                }
+                _array1[_array1.Length - 1] = value;
+            }
+            
+            
+        }
+        public int Count { get { return _array.Length; } }
+        public int Count1 { get { return _array1.Length; } }
+
+        public void Yazdir()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine(Convert.ToString(_array[i]) + " " + _array1[i]);
+
+            }
+        }
+        
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyDictionary // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+            Console.Write("Eklemek istediğiniz Kişi Sayısını Giriniz =");
+            int sayi1=Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <sayi1 ; i++)
+            {
+              
+
+                Console.Write("bir sayi giriniz = ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Bir isim giriniz = ");
+                string isim = Console.ReadLine();
+                myDictionary.Add(sayi, isim);
+            }
+            myDictionary.Yazdir();
+        }
+    }
+    
+}
